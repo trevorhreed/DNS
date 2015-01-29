382734d86041c2fa69c295a21120ef97c7326bbe
Trevor Reed <trevor.reed@byu.edu>
2015-01-29 16:10:41 -0700

    adding upload demo, and initial project code

---
+++ mvc/Models/FormModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace mvc.Models
+{
+    public class FormModel
+    {
+        public String Name { get; set; }
+        public List<PhoneNumber> Contacts { get; set; }
+
+        public FormModel()
+        {
+            Contacts = new List<PhoneNumber>();
+        }
+    }
+
+    public class PhoneNumber
+    {
+        public String Number { get; set; }
+        public String Extension { get; set; }
+    }
+}
