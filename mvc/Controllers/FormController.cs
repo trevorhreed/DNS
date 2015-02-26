8e8e9444bb3d4e038191a62c587b32b561cc18ab
Trevor Reed <trevor.reed@byu.edu>
2015-02-25 17:52:06 -0700

    updating mvc with image uploading

---
+++ mvc/Controllers/FormController.cs
@@ -0,0 +1,59 @@
+﻿using log4net;
+using mvc.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvc.Controllers
+{
+    public class FormController : Controller
+    {
+        private ILog log = LogManager.GetLogger(typeof(FormController));
+
+        public ActionResult Index()
+        {
+            log.Debug("Enter Index");
+
+            FormModel model = new FormModel();
+            model.Name = "Harrison";
+			
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Index(FormModel model)
+        {
+            log.Debug("Enter Index Post");
+
+            if (ModelState.IsValid)
+            {
+                if (model.Image != null && model.Image.ContentLength > 0)
+                {
+                    model.Image.SaveAs(@"C:\dump\" + model.Name + "_" + model.Image.FileName);
+                }
+				if (model.ImageThin != null && model.ImageThin.ContentLength > 0)
+				{
+					model.ImageThin.SaveAs(@"C:\dump\" + model.Name + "_thin_" + model.ImageThin.FileName);
+				}
+
+				StringBuilder sb = new StringBuilder();
+                sb.AppendLine("Name: " + model.Name);
+                sb.AppendLine();
+                using (StreamWriter outfile = new StreamWriter(@"C:\dump\" + model.Name + "_info.txt"))
+                {
+                    outfile.Write(sb.ToString());
+                }
+            }
+
+            return RedirectToAction("Index");
+        }
+
+		public ActionResult NewSocialLink()
+		{
+			return PartialView("_SocialLinkForm");
+		}
+    }
+}
---
+++ mvc/Models/Form/FormModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace mvc.Models
+{
+    public class FormModel
+    {
+        public String Name { get; set; }
+		public HttpPostedFileBase Image { get; set; }
+		public HttpPostedFileBase ImageThin { get; set; }
+	}
+}
