7ef22fabfb3344d0778845f687a0aa4b3dab8747
Trevor Reed <trevor.reed@byu.edu>
2015-05-14 18:03:59 -0600

    added async file reading to jdb

--- jdb/Controllers/HomeController.cs
+++ jdb/Controllers/HomeController.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
 
 namespace jdb.Controllers
 {
@@ -17,19 +19,60 @@ namespace jdb.Controllers
 
 		public object People()
 		{
+			string DIR_PATH = Path.Combine(Request.PhysicalApplicationPath, "App_Data/people");
+
 			Stopwatch watch = Stopwatch.StartNew();
-			var json = "[";
-			var DIR = Path.Combine(Request.PhysicalApplicationPath, "App_Data/people");
-			foreach (string file in Directory.EnumerateFiles(DIR))
-			{
-				if (json != "[") json += ",";
-				json += System.IO.File.ReadAllText(file);
-				
-			}
-			json += "]";
-			var time = (float)watch.ElapsedMilliseconds / (float)1000;
-			json = "{ \"time\": " + time + ", \"data\": " + json + " }";
-			return json;
+			var content1 = ReadAllFilesSync(DIR_PATH);
+			var time1 = (float)watch.ElapsedMilliseconds / (float)1000;
+			watch.Restart();
+			var content2 = ReadAllFilesAsync(DIR_PATH);
+			var time2 = (float)watch.ElapsedMilliseconds / (float)1000;
+
+			Dictionary<string, dynamic> json = new Dictionary<string, dynamic>();
+			json.Add("Sync", time1);
+			json.Add("Async", time2);
+			return new JavaScriptSerializer().Serialize(json);
 		}
+
+		private string ReadAllFilesSync(string directoryPath)
+		{
+			List<string> contents = new List<string>();
+			string[] files = Directory.GetFiles(directoryPath);
+			for (var i = 0; i < files.Length; i++)
+			{
+				contents.Add(System.IO.File.ReadAllText(files[i]));
+			}
+			return "[" + string.Join(",", contents) + "]";
+		}
+
+		private async Task<string> ReadFileAsync(string file)
+		{
+			string content = "";
+			using (FileStream stream = System.IO.File.Open(file, FileMode.Open))
+			{
+				byte[] result = new byte[stream.Length];
+				await stream.ReadAsync(result, 0, (int)stream.Length);
+				content = System.Text.Encoding.UTF8.GetString(result);
+			}
+			return content;
+		}
+
+		private async Task<string> ReadAllFilesAsync(string directoryPath)
+		{
+			List<string> contents = new List<string>();
+			string[] files = Directory.GetFiles(directoryPath);
+			Task<string>[] tasks = new Task<string>[files.Length];
+			for(var i=0; i < files.Length; i++)
+			{
+				tasks[i] = ReadFileAsync(files[i]);
+			}
+			for (var i = 0; i < tasks.Length; i++)
+			{
+				await tasks[i];
+				contents.Add(tasks[i].Result);
+			}
+			return "[" + string.Join(",", contents) + "]";
+		}
+
     }
 }
